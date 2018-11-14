ba0bed8775a600ff1dcc25247268903d2a6f5c2c
vineethariharan <gaurav.mahajan@students.iiit.ac.in>
2018-11-14 14:09:27 +0530

    pushing code to convert mlabs data to convert into trie ds

--- MlabsToFile/mlabs_to_file.py
+++ MlabsToFile/mlabs_to_file.py
@@ -4,7 +4,18 @@ client = pymongo.MongoClient('mongodb://user:password123@ds123963.mlab.com:23963
 db = client.get_default_database()
 flipkart_data = db.flipkart_data
 
+f = open("op.txt", "w")
+
+
+
 
 for document in flipkart_data.find():
     print(document) # iterate the cursor
+    for key in document:
+        if(key == "model name"):
+            f.write(str(document[key]) + "\t" + "model\n");
+        else:
+            f.write(key + "\t" + "attribute\n")
+            f.write(str(document[key]) + "\t" + "data\n")
+
 k = 0;
